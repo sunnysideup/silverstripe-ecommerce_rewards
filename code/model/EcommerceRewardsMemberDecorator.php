62098c91325a41937555d1775891c0d6e1973a3a
helpfulrobot <chris@silverstripe.com>
2016-01-01 00:17:02 +1300

    Converted to PSR-2

--- code/model/EcommerceRewardsMemberDecorator.php
+++ code/model/EcommerceRewardsMemberDecorator.php
@@ -1,42 +1,45 @@
 <?php
 
-class EcommerceRewardsMemberDecorator extends DataObjectDecorator {
-	public function extraStatics() {
-		return array (
-			'db' => array (
-				'PointsBalance' => 'Currency'
-			)
-		);
-	}
+class EcommerceRewardsMemberDecorator extends DataObjectDecorator
+{
+    public function extraStatics()
+    {
+        return array(
+            'db' => array(
+                'PointsBalance' => 'Currency'
+            )
+        );
+    }
 
-	function updateCMSFields(&$fields) {
-		$fields->addFieldToTab("Root.Points", new ReadonlyField("PointsBalance"));
-	}
+    public function updateCMSFields(&$fields)
+    {
+        $fields->addFieldToTab("Root.Points", new ReadonlyField("PointsBalance"));
+    }
 
-	private $lastPoints = 0;
+    private $lastPoints = 0;
 
-	function onBeforeWrite(){
-		$this->lastPoints = $this->owner->PointsBalance;
-	}
+    public function onBeforeWrite()
+    {
+        $this->lastPoints = $this->owner->PointsBalance;
+    }
 
-	function onAfterWrite(){
-		if(!$this->lastPoints != $this->owner->PointsBalance) {
-			$obj = new EcommerceRewardsMemberDecorator_Log();
-			$obj->PreviousValue = $this->lastPoints;
-			$obj->CurrentValue = $this->owner->PointsBalance;
-			$obj->write();
-		}
-	}
-
-
+    public function onAfterWrite()
+    {
+        if (!$this->lastPoints != $this->owner->PointsBalance) {
+            $obj = new EcommerceRewardsMemberDecorator_Log();
+            $obj->PreviousValue = $this->lastPoints;
+            $obj->CurrentValue = $this->owner->PointsBalance;
+            $obj->write();
+        }
+    }
 }
 
 
-class EcommerceRewardsMemberDecorator_Log extends DataObject {
+class EcommerceRewardsMemberDecorator_Log extends DataObject
+{
 
-	static $db = array(
-		"PreviousValue" => "Currency",
-		"CurrentValue" => "Currency"
-	);
-
+    public static $db = array(
+        "PreviousValue" => "Currency",
+        "CurrentValue" => "Currency"
+    );
 }
--- code/model/EcommerceRewardsSiteConfigDecorator.php
+++ code/model/EcommerceRewardsSiteConfigDecorator.php
@@ -6,46 +6,49 @@
  *
  **/
 
-class EcommerceRewardsSiteConfigDecorator extends DataObjectDecorator {
+class EcommerceRewardsSiteConfigDecorator extends DataObjectDecorator
+{
 
-	function extraStatics(){
-		return array(
-			'db' => array(
-				'PointsExchangeRate' => 'Currency',
-			),
-		);
-	}
-
-
-	function updateCMSFields(FieldSet &$fields) {
-		$fields->addFieldToTab('Root.RewardPoints', new NumericField('PointsExchangeRate', 'The number of dollars a customer has to spend to receive one reward point.'));
-		return $fields;
-	}
-
-	private $lastPointsExchangeRate = 0;
-
-	function onBeforeWrite(){
-		$this->lastPointsExchangeRate = $this->owner->PointsExchangeRate;
-	}
-
-	function onAfterWrite(){
-		if(!$this->lastPointsExchangeRate != $this->owner->PointsExchangeRate) {
-			$obj = new EcommerceRewardsSiteConfigDecorator_Log();
-			$obj->PreviousValue = $this->lastPointsExchangeRate;
-			$obj->CurrentValue = $this->owner->PointsExchangeRate;
-			$obj->write();
-		}
-	}
+    public function extraStatics()
+    {
+        return array(
+            'db' => array(
+                'PointsExchangeRate' => 'Currency',
+            ),
+        );
+    }
 
 
+    public function updateCMSFields(FieldSet &$fields)
+    {
+        $fields->addFieldToTab('Root.RewardPoints', new NumericField('PointsExchangeRate', 'The number of dollars a customer has to spend to receive one reward point.'));
+        return $fields;
+    }
+
+    private $lastPointsExchangeRate = 0;
+
+    public function onBeforeWrite()
+    {
+        $this->lastPointsExchangeRate = $this->owner->PointsExchangeRate;
+    }
+
+    public function onAfterWrite()
+    {
+        if (!$this->lastPointsExchangeRate != $this->owner->PointsExchangeRate) {
+            $obj = new EcommerceRewardsSiteConfigDecorator_Log();
+            $obj->PreviousValue = $this->lastPointsExchangeRate;
+            $obj->CurrentValue = $this->owner->PointsExchangeRate;
+            $obj->write();
+        }
+    }
 }
 
 
-class EcommerceRewardsSiteConfigDecorator_Log extends DataObject {
+class EcommerceRewardsSiteConfigDecorator_Log extends DataObject
+{
 
-	static $db = array(
-		"PreviousValue" => "Currency",
-		"CurrentValue" => "Currency"
-	);
-
+    public static $db = array(
+        "PreviousValue" => "Currency",
+        "CurrentValue" => "Currency"
+    );
 }
